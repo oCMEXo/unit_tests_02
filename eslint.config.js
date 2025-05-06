d4030a61e5735b2a02fe275ca6d7c7272b73adef
artem <artsiomharminovich05@gmail.com>
2025-05-06 13:03:09 +0300

    fix file HM2

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,20 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: 2021,
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
   pluginReact.configs.flat.recommended,
-    //123
 ]);
