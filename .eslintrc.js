1f0dd212483519c64355638cb0af6782cd93dded
artem <artsiomharminovich05@gmail.com>
2025-05-06 15:13:32 +0300

    fix hm2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  env: {
-    node: true, // Добавляет глобальные переменные для Node.js
-    es2021: true, // Поддержка синтаксиса ECMAScript 2021
-  },
-  extends: [
-    'eslint:recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    // Ваши правила здесь
-  },
+
+    'env': {
+
+        mocha: true,
+        node: true,
+
+    },
+    plugins: ["mocha"],
+    rules: {
+        "mocha/no-exclusive-tests": "error",
+    },
+
+
 };
